5090fea2d6c2b43e5ff6af0b147d87cb40e53a51
aa1111111 <1985382199@qq.com>
2021-11-19 14:39:39 +0800

    防疫系统

---
+++ Epidemic-prevention-web - 副本/src/views/index/components/HorsesLamp.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="pic">
+     <el-carousel :interval="4000" type="card" :height="bannerHeight+'px'">
+          <el-carousel-item v-for="item in imgUrls" :key="item.id">
+            <el-row>
+                <el-col :span="24" class="banner_img">
+                    <img ref="bannerHeight" :src="item.idView" class="bannerImg" @load="imgLoad"  height="250px"/>
+                </el-col>
+            </el-row>
+          </el-carousel-item>
+     </el-carousel>
+</div>
+</template>
+<script>
+export default {
+    name:"HorseLamp",
+    data() {
+    return {
+        bannerHeight:"",
+      imgUrls: [
+        {id:0,idView:require('../../../assets/public_images/MD1.png')},
+        {id:1,idView:require('../../../assets/public_images/MD2.png')},
+        {id:2,idView:require('../../../assets/public_images/MD3.png')},
+      ]
+    }
+  },
+  methods:{
+      imgLoad(){
+          this.$nextTick(()=>{
+              this.bannerHeight=this.$refs.bannerHeight[0].height
+              console.log(this.$refs.bannerHeight[0].height);
+          })
+      }
+  },
+  mounted(){
+      this.imgLoad();
+      window.addEventListener('resize',()=>{
+          this.bannerHeight=this.$refs.bannerHeight[0].height
+            this.imgLoad();
+          },false)
+      },
+}
+</script>
+<style scoped>
+.pic{
+    margin-left: 55px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+</style>
